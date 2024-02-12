c46c780cdaf86d907906a75f5a6612ea42708dae
miguelr215 <drexelramos@gmail.com>
2024-02-12 14:57:10 -0500

    cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const questions = [
+  {
+    id: 1234,
+    question: 'What language is React based on?',
+    answer: 'JavaScript',
+  },
+  {
+    id: 4321,
+    question: 'What are the building blocks of React apps?',
+    answer: 'Components',
+  },
+  {
+    id: 5678,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: 'JSX',
+  },
+  {
+    id: 8765,
+    question: 'How to pass data from parent to child components?',
+    answer: 'props',
+  },
+  {
+    id: 9012,
+    question: 'How to give components memory?',
+    answer: 'useState hook',
+  },
+  {
+    id: 2109,
+    question:
+      'What do we call an input element that is completely synchronized with state?',
+    answer: 'Controlled Element',
+  },
+  {
+    id: 3456,
+    question: 'When you need data to change within a component, use ____?',
+    answer: 'State',
+  },
+  {
+    id: 6543,
+    question:
+      'When you need to define the "settings" of a component, use ____?',
+    answer: 'props',
+  },
+  {
+    id: 1212,
+    question: 'React will rerender the UI when the ____ is changed',
+    answer: 'state',
+  },
+];
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlashCards />
     </div>
   );
 }
 
-export default App;
+function FlashCards() {
+  const [selectedID, setSelectedID] = useState(null);
+
+  function handleClick(id) {
+    setSelectedID(id !== selectedID ? id : null);
+  }
+
+  return (
+    <ul>
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          className={`card ${selectedID === question.id ? 'answered' : ''}`}
+          onClick={() => handleClick(question.id)}
+        >
+          <p>
+            {selectedID === question.id ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </ul>
+  );
+}
